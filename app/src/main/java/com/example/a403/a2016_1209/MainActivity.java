c4eb90e8fb6f8d0cead724c74dd47410d32d1350
leeym008 <leeym008@naver.com>
2016-12-09 18:40:39 +0900

    P1 레이아웃 생성

--- app/src/main/java/com/example/a403/a2016_1209/MainActivity.java
+++ app/src/main/java/com/example/a403/a2016_1209/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.a403.a2016_1209;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button BTN1, BTN;
+    TextView TV1, TV2, TV3;
+    EditText ET1, ET2, ET3;
+    ImageView IV1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
